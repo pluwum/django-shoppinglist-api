627c7d9a18aac4ce8c22f1e8b3145482a69bfa37
pluwum <luwyxx@gmail.com>
2019-02-14 22:06:52 +0300

    (feature) Get shopping list items in shoppinglist.

--- shoppinglist/serializers.py
+++ shoppinglist/serializers.py
@@ -2,14 +2,15 @@ from rest_framework import serializers
 from .models import ShoppingList, ShoppingListItem
 
 
-class ShoppingListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = ShoppingList
-        fields = '__all__'
-
-
 class ShoppingListItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingListItem
+        fields = '__all__'
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    items = ShoppingListItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ShoppingList
         fields = '__all__'
