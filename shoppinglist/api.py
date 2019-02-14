4399e6737ba87d195d72a00884c7a7ba390a3f78
pluwum <luwyxx@gmail.com>
2019-02-14 23:02:29 +0300

    (feature) Get shopping list items only when retrieving a shopping list.

--- shoppinglist/api.py
+++ shoppinglist/api.py
@@ -1,11 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
-from .serializers import ShoppingListSerializer, ShoppingListItemSerializer
+from .serializers import ShoppingListSerializer, ShoppingListItemSerializer, ShoppingListDetailSerializer
 from .models import ShoppingList, ShoppingListItem
+from rest_framework.response import Response
 
 
 class ShoppingListViewSet(ModelViewSet):
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
+
+    def retrieve(self, request, pk=None):
+        serializer = ShoppingListDetailSerializer(self.queryset, many=True)
+        return Response(serializer.data)
 
 
 class ShoppingListItemViewSet(ModelViewSet):
--- shoppinglist/serializers.py
+++ shoppinglist/serializers.py
@@ -8,8 +8,15 @@ class ShoppingListItemSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ShoppingListSerializer(serializers.ModelSerializer):
+class ShoppingListDetailSerializer(serializers.ModelSerializer):
     items = ShoppingListItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ShoppingList
+        fields = '__all__'
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ShoppingList
