eaceb7c39573dc5990845ec0c1f885188186ed24
pluwum <luwyxx@gmail.com>
2019-02-14 17:57:35 +0300

    (feature) Get all shoppinglists.

--- shoppinglist/models.py
+++ shoppinglist/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 
 
+class ShoppingList(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
